634709d221d8ab394aca01da272c234b4a1163f0
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-05-26 13:58:04 -0400

    Cors

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
